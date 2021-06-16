03e76ea7c758f15e1633a9d45d13d8795e8d03c9
Gupanets Dmytro <dmgpnts@gmail.com>
2021-06-16 13:26:19 +0300

    completed hw-10

--- src/index.js
+++ src/index.js
@@ -1,1 +1,56 @@
-import './sass/main.scss';
+'use strict';
+import menuList from './menu.json';
+import cardTemplate from './partials/card-template.hbs';
+const Handlebars = require('handlebars');
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const defaultUserThemeValue = JSON.parse(localStorage.getItem('Theme'));
+const themeSwitchRef = document.querySelector('#theme-switch-toggle');
+const menuCompiledList = menuItemListCreate();
+const menuListRef = document.querySelector('.js-menu');
+
+themeSwitchRef.addEventListener('change', onSwitchTheme);
+
+defineUserThemeSettings(defaultUserThemeValue);
+
+function defineUserThemeSettings(value) {
+  if (value === 'light-theme') return;
+
+  if (value === 'dark-theme') onSwitchToDarkTheme();
+}
+
+function onSwitchTheme() {
+  if (document.body.classList.contains('light-theme')) {
+    onSwitchToDarkTheme();
+    return;
+  }
+
+  if (document.body.classList.contains('dark-theme')) {
+    onSwitchToLightTheme();
+    return;
+  }
+}
+
+function onSwitchToDarkTheme() {
+  document.body.classList.replace('light-theme', 'dark-theme');
+  themeSwitchRef.checked = true;
+  localStorage.setItem('Theme', JSON.stringify(Theme.DARK));
+}
+
+function onSwitchToLightTheme() {
+  document.body.classList.replace('dark-theme', 'light-theme');
+  themeSwitchRef.checked = false;
+  localStorage.setItem('Theme', JSON.stringify(Theme.LIGHT));
+}
+
+// =============================================================
+
+function menuItemListCreate() {
+  return menuList.map(cardTemplate).join('');
+}
+
+menuListRef.insertAdjacentHTML('beforeend', menuCompiledList);
